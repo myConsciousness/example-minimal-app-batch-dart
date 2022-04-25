91fbcdbb7ab67cb9ca22c29f5d172db01e001f22
myConsciousness <kato.shinya.dev@gmail.com>
2022-04-25 09:41:47 +0900

    add

---
+++ bin/minimal_batch_app.dart
@@ -0,0 +1,31 @@
+import 'package:batch/batch.dart';
+
+void main(List<String> args) => runWorkflow(
+      jobs: [SayHelloWorldJob()],
+    );
+
+class SayHelloWorldJob implements ScheduledJobBuilder {
+  @override
+  ScheduledJob build() => ScheduledJob(
+        name: 'Say Hello World Job',
+        schedule: CronParser('*/2 * * * *'), // 2分毎に実行
+        steps: [
+          Step(name: 'Say Hello Step', task: SayHelloTask()),
+          Step(name: 'Say World Step', task: SayWorldTask())
+        ],
+      );
+}
+
+class SayHelloTask extends Task<SayHelloTask> {
+  @override
+  void execute(ExecutionContext context) {
+    log.info('Hello, ');
+  }
+}
+
+class SayWorldTask extends Task<SayHelloTask> {
+  @override
+  void execute(ExecutionContext context) {
+    log.info('World!');
+  }
+}
